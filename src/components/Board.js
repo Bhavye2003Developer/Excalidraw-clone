9a0bed6e4347ee92a16599a5b759a3a4296304b2
Bhavye <bhavyedevelopment2003@gmail.com>
2024-04-21 14:24:46 +0530

    excalidraw init

---
+++ src/components/Board.js
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from "react";
+
+const Board = () => {
+  const [canvasContext, setCanvasContext] = useState(null);
+  const [startPos, setStartPos] = useState([null, null]);
+  const [endPos, setEndPos] = useState([null, null]);
+  const [isPathInitiated, setIsPathInitiated] = useState(false);
+  const [cursorPos, setCursorPos] = useState([null, null]);
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    setCanvasContext(canvasRef.current.getContext("2d"));
+  }, []);
+
+  useEffect(() => {
+    if (startPos[0] && startPos[1]) {
+      if (canvasContext) {
+        canvasContext.beginPath();
+        canvasContext.moveTo(startPos[0], startPos[1]);
+        setIsPathInitiated(true);
+        console.log("path initiated...");
+      }
+    }
+  }, [startPos]);
+
+  useEffect(() => {
+    if (endPos[0] && endPos[1]) {
+      if (canvasContext) {
+        setIsPathInitiated(false);
+        setCursorPos([null, null]);
+        console.log("path terminated");
+        canvasContext.lineTo(endPos[0], endPos[1]);
+        // make line till x, y
+        canvasContext.stroke();
+      }
+    }
+  }, [endPos]);
+
+  const getRelativePointCoordinates = (x, y) => {
+    const x0 = canvasRef.current.getBoundingClientRect().left;
+    const y0 = canvasRef.current.getBoundingClientRect().top;
+    const xNew = x - x0;
+    const yNew = y - y0;
+    return [xNew, yNew];
+  };
+
+  useEffect(() => {
+    if (cursorPos[0] && cursorPos[1]) {
+      // path initiated and cursor moving
+      // canvasContext.moveTo(cursorPos[0], cursorPos[1]);
+    }
+  }, cursorPos);
+
+  return (
+    <div>
+      <center>
+        <canvas
+          id="canvas"
+          ref={canvasRef}
+          width={600}
+          height={300}
+          style={{
+            backgroundColor: "red",
+          }}
+          onMouseMove={(e) => {
+            setCursorPos(getRelativePointCoordinates(e.clientX, e.clientY));
+          }}
+          onClick={(e) => {
+            if (!isPathInitiated)
+              setStartPos(getRelativePointCoordinates(e.clientX, e.clientY));
+            else setEndPos(getRelativePointCoordinates(e.clientX, e.clientY));
+          }}
+          tabIndex={0}
+        ></canvas>
+      </center>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import Board from "./Board";
+
+const Body = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
+
+export default Body;
